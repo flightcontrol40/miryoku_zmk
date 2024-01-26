ccdce8243bbeebd904cf6eba7879b202df3a72a1
Nathan Hampton <nathanhampton40000@gmail.com>
2024-01-26 05:48:40 -0800

    Corne Updates

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,5 +1,5 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_KLUDGE_TAPDELAY 
-
+// #define MIRYOKU_KLUDGE_TAPDELAY
+#define MIRYOKU_ALPHAS_QWERTY_CUSTOM
